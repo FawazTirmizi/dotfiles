4fe0507b57cac6de8b3eb2ed72db7f572d16c714
Fawaz <fawaztirmizi@outlook.com>
2022-01-20 10:03:28 -0600

    Made changes on work laptop, added and cleaned up scripts

--- packages.sh
+++ packages.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function check_clone() {
+    [ ! -d "$2" ] && git clone --recurse-submodules $1 $2
+}
+
 # Packages
 
 
@@ -7,12 +11,12 @@
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
 # Mold
-apt-get update
-apt-get install -y build-essential git clang cmake libstdc++-10-dev libssl-dev libxxhash-dev zlib1g-dev
-git clone git@github.com:rui314/mold.git
+sudo apt-get update
+sudo apt-get install -y build-essential git clang cmake libstdc++-10-dev libssl-dev libxxhash-dev zlib1g-dev
+check_clone git@github.com:rui314/mold.git $HOME/utils/mold
 
 # Alacritty
-apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-git clone git@github.com:alacritty/alacritty.git
+sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+git clone git@github.com:alacritty/alacritty.git $HOME/utils/alacritty
 
 
--- setup.sh
+++ setup.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 
-function check_make_dir() {
-    [ ! -d "$1" ] && mkdir $1
-}
-
-while getopts 'grb' FLAG; do
+while getopts 'grbcp' FLAG; do
     case "${FLAG}" in
         g) ./git.sh ;;
         r) ./repos.sh ;;
         b) cat bashrc >> $HOME/.bashrc ;;
+	    c) ./link_configs.sh ;;
+        p) ./packages.sh ;;
     esac
 done
-
-# Add .vimrc
-ln ./vimrc $HOME/.vimrc
-
-CONFIG=$HOME/.config/
-
-check_make_dir $CONFIG/i3
-ln i3-config $CONFIG/i3/config
-
-check_make_dir $CONFIG/polybar
-ln polybar-config $CONFIG/polybar/config
-
-ln alacritty.yml $HOME/.alacritty.yml
-
-
 
 
 
